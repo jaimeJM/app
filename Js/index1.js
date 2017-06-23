61c0781835b0188dc674f2704968d1573bb68d8f
jaimeJM <jaimemartinezqa@gmail.com>
2017-06-23 13:20:27 -0400

    primer commit

--- Js/index1.js
+++ Js/index1.js
@@ -5,7 +5,7 @@ var id = setInterval(frame, 110);
 function frame(){
 	if (loading == 100) {
 		clearInterval(id);
-		window.open("C:/Users/JM/Desktop/aplicacion/Html/principal.html", "_self");
+		window.open("../Html/principal.html", "_self");
 		}
 		else {
 			loading = loading + 1;
